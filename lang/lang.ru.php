b6b1b85ce15e09a824b1bddf51c5495fc679bf93
andrrr <andrey.lubinov@gmail.com>
2010-02-15 20:17:54 +0800

    Setup Page localisation

--- lang/lang.ru.php
+++ lang/lang.ru.php
@@ -19,7 +19,7 @@
 	$dictionary = array(
 	
 		'Members' =>
-		'Пользователи',
+		'Система пользователей',
 		
 		'Roles' =>
 		'Роли',
@@ -31,7 +31,7 @@
 		'Установки',
 		
 		'Symphony &ndash; Members &ndash; Setup' =>
-		'Symphony &ndash; Пользователи &ndash; Установки',
+		'Symphony &ndash; Система пользователей &ndash; Установки',
 		
 		'Username and Password are required fields.' =>
 		'Имя пользователя и пароль — обязательные для заполнения поля',
@@ -41,6 +41,54 @@
 
 		'Member: Username &amp; Password' =>
 		'Пользователь: имя пользователя и пароль',
+
+		'Instructions' => 
+		'Инструкции',
+		
+		'A section is required for storing member details. Use the button below to automatically create a compatible section (you can always edit or add fields later), or use
+the dropdowns to link to an existing section containing the required fields.' => 
+		'Для хранения пользовательских данных требуется секция.<br/>Используйте <strong>автоустановку</strong>, чтобы автоматически создать совместимую секцию (вы всегда сможете отредактировать или
+добавить поля позже) <br/>или выберите поля уже существующей секции в выпадающих меню ниже.',
+		
+		'Smart Setup' => 
+		'Автоустановка',
+		
+		'Automatically creates a new section, called Members, containing Username/Password, Role, Email Address, and Timezone Offset fields.' => 
+		'Автоматически создаст новую секцию под названием Members, содержащую поля Username/Password, Role, Email Address и Timezone Offset.',
+		
+		'Must contain a <code>Member</code> type field. Will be used to validate login details.' => 
+		'Должно присутствовать поле <code>Member</code>,&nbsp;которое используется для проверки пользовательских данных.',
+		
+		'Member Section' => 
+		'Секция с данными пользователей',
+		
+		'Email Address Field' => 
+		'Поле c адресом Email',
+		
+		'Must set Member section first' => 
+		'Сначала настройте секцию с данными пользователей',
+		
+		'Timezone Offset Field' => 
+		'Поле с временной зоной',
+		
+		'Stores member timezones. Used to dynamically adjust date displays. Defaults to Symphony configuration offset.' => 
+		'Содержит временные зоны пользователей. Используется для динамической подстройки отображения дат. Значение по умолчанию берется из ситемной конфигурации Symphony.',
+		
+		'Registration' => 
+		'Регистрация',
+		
+		'New Member Default Role' => 
+		'Роль, устанавливаемая новому пользователю по умолчанию',
+		
+		'New members require activation' => 
+		'Требуется активация',
+		
+		'If activation is required, new members will be added to the \'Inactive\' role until they reply to the \'Activate Account\' email. Activated members will be added to the role selected above.' => 
+		'Если требуется активация, новый пользователь будет в роли \'Inactive\' до тех пор, пока не ответит на email, активирующий его аккаунт.<br/> Активированные пользователи получают роль
+определенную выше.',
+		
+		'' => 
+		'',
 
 	);
 	
