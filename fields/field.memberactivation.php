d39932529bf758fe148c5cd07727ffbbf965228e
Rowan Lewis <me@rowanlewis.com>
2011-03-15 12:56:57 +1000

    Missing the ORDER BY part of the SQL statement in buildSortingSQL.

--- fields/field.memberactivation.php
+++ fields/field.memberactivation.php
@@ -392,7 +392,7 @@
 
 		public function buildSortingSQL(&$joins, &$where, &$sort, $order='ASC') {
 			$joins .= "INNER JOIN `tbl_entries_data_".$this->get('id')."` AS `ed` ON (`e`.`id` = `ed`.`entry_id`) ";
-			$sort .= (strtolower($order) == 'random' ? 'RAND()' : "`ed`.`activated` $order");
+			$sort .= 'ORDER BY ' . (strtolower($order) == 'random' ? 'RAND()' : "`ed`.`activated` $order");
 		}
 
 	/*-------------------------------------------------------------------------
