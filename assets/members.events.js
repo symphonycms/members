f4886c84415eb2c8281bdce1c21de7335480afa5
Brendan Abbott <brendan@randb.com.au>
2011-05-18 20:03:16 +1000

    Use Symphony.Message.post for the Email Template saving message's

--- assets/members.events.js
+++ assets/members.events.js
@@ -2,29 +2,6 @@ jQuery(document).ready(function() {
 	var EVENTS_NEW_URL = Symphony.Context.get('root') + '/symphony/blueprints/events/new/',
 		EVENTS_URL = Symphony.Context.get('root') + '/symphony/blueprints/components/';
 
-	// Given a type (success/error), this function will add to the notice
-	// at the top of the Symphony (or create one if it does not exist)
-	var addPageNotice = function(type, message) {
-		var $header = jQuery("#header"),
-			$notice = jQuery("p#notice");
-
-		if($notice.length) {
-			$notice
-				.addClass(type)
-				.html(message);
-		}
-		else {
-			$header.prepend(
-				jQuery('<p />')
-					.attr('id', 'notice')
-					.addClass(type)
-					.html(message)
-			);
-		}
-
-		return false;
-	}
-
 	// Save our the Event's email preference
 	jQuery('form').live('submit', function() {
 		jQuery.ajax({
@@ -34,14 +11,14 @@ jQuery(document).ready(function() {
 			data: jQuery('form').serialize(),
 			success: function(data, response) {
 				if(response == "success") {
-					addPageNotice('success', 'Event updated. <a href="' + EVENTS_NEW_URL + '" accesskey="c">Create another?</a> <a href="' + EVENTS_URL + '" accesskey="a">View all Events</a>');
+					Symphony.Message.post('Event updated. <a href="' + EVENTS_NEW_URL + '" accesskey="c">Create another?</a> <a href="' + EVENTS_URL + '" accesskey="a">View all Events</a>', 'success');
 				}
 				else {
-					addPageNotice('error','An error occurred while processing this form.');
+					Symphony.Message.post('An error occurred while processing this form.', 'error');
 				}
 			},
 			error: function(data, response) {
-				addPageNotice('error', 'An error occurred while processing this form.');
+				Symphony.Message.post('An error occurred while processing this form.', 'error');
 			}
 		});
 
