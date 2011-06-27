4d29dd1e452dbd431861321c463c1244d8e3f0b2
Simone Economo <my.ekoes@gmail.com>
2011-06-27 20:39:47 +0200

    Localization fixes

--- fields/field.memberusername.php
+++ fields/field.memberusername.php
@@ -71,7 +71,7 @@
 
 			if(empty($username)) {
 				extension_Members::$_errors[$this->get('element_name')] = array(
-					'message' => __('%s is a required field.', array($this->get('label'))),
+					'message' => __('\'%s\' is a required field.', array($this->get('label'))),
 					'type' => 'missing',
 					'label' => $this->get('label')
 				);
@@ -149,7 +149,7 @@
 
 			//	If the field is required
 			if(($this->get('required') == "yes") && empty($username)) {
-				$message = __('%s is a required field.', array($this->get('label')));
+				$message = __('\'%s\' is a required field.', array($this->get('label')));
 				return self::__MISSING_FIELDS__;
 			}
 
