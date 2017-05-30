e80329a76d0d2831be2ed6f2fb6c62b997fea6d5
Alexandra Nantel <alexandra@deuxhuithuit.com>
2017-05-30 14:27:02 -0400

    Remove deprecated param from buildSortingSQL

--- fields/field.memberrole.php
+++ fields/field.memberrole.php
@@ -392,7 +392,7 @@
 		Sorting:
 	-------------------------------------------------------------------------*/
 
-		public function buildSortingSQL(&$joins, &$where, &$sort, $order='ASC', &$select = NULL) {
+		public function buildSortingSQL(&$joins, &$where, &$sort, $order='ASC') {
 			if(in_array(strtolower($order), array('random', 'rand'))) {
 				$sort = 'ORDER BY RAND()';
 			}
